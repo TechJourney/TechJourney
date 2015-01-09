e80a49e834346dfc94e7a7c98c219d5ef7c64655
joe-l-bright <keljoebright@gmail.com>
2015-01-08 18:02:36 -0600

    update to phonegap project

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,5 @@
 var pictureSource;   // picture source
 var destinationType; // sets the format of returned value
-// The watch id references the current `watchAcceleration`
-var watchID = null;
 
 // Wait for device API libraries to load
 //
@@ -12,50 +10,13 @@ document.addEventListener("deviceready",onDeviceReady,false);
 function onDeviceReady() {
     pictureSource=navigator.camera.PictureSourceType;
     destinationType=navigator.camera.DestinationType;
-    startWatch();
 }
-
-// Start watching the acceleration
-//
-function startWatch() {
-
-    // Update acceleration every 3 seconds
-    var options = { frequency: 3000 };
-
-    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-}
-
-// Stop watching the acceleration
-//
-function stopWatch() {
-    if (watchID) {
-        navigator.accelerometer.clearWatch(watchID);
-        watchID = null;
-    }
-}
-
-// onSuccess: Get a snapshot of the current acceleration
-//
-function onSuccess(acceleration) {
-    var element = document.getElementById('accelerometer');
-    element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
-    'Acceleration Y: ' + acceleration.y         + '<br />' +
-    'Acceleration Z: ' + acceleration.z         + '<br />' +
-    'Timestamp: '      + acceleration.timestamp + '<br />';
-}
-
-// onError: Failed to get the acceleration
-//
-function onError() {
-    alert('onError!');
-}
-
 
 // Called when a photo is successfully retrieved
 //
-function onPhotoDataSuccess(imageData) {
+function smallImageSuccess(imageData) {
     // Uncomment to view the base64-encoded image data
-    console.log(imageData);
+    // console.log(imageData);
 
     // Get image handle
     //
@@ -67,11 +28,27 @@ function onPhotoDataSuccess(imageData) {
     smallImage.src = "data:image/jpeg;base64," + imageData;
 }
 
+// Called when a photo is successfully retrieved
+//
+function largeImageSuccess(imageData) {
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
+
+    // Show the captured photo
+    // The in-line CSS rules are used to resize the image
+    //
+    largeImage.src = "data:image/jpeg;base64," + imageData;
+}
+
 // A button will call this function
 //
 function capturePhoto() {
     // Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20,
+    navigator.camera.getPicture(smallImageSuccess, onFail, { quality: 50,
         destinationType: destinationType.DATA_URL });
 }
 
@@ -79,7 +56,7 @@ function capturePhoto() {
 //
 function capturePhotoEdit() {
     // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+    navigator.camera.getPicture(largeImageSuccess, onFail, { quality: 20, allowEdit: true,
         destinationType: destinationType.DATA_URL });
 }
 
